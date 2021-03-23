a9d5e96ac0a4547d09021fe806e6837d45523da4
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2021-03-23 16:05:14 -0400

    Update layout

--- src/components/SideNavigation.js
+++ src/components/SideNavigation.js
@@ -4,9 +4,21 @@ const SideNavigation = () => {
     return (
         <div className="side-nav">
             <ul className="navigation">
-                <li className="navigation__item">Flex-Direction</li>
-                <li className="navigation__item">Flex-Direction</li>
-                <li className="navigation__item">Flex-Direction</li>
+                <li className="navigation__item">
+                    <a href="#" class="navigation__link">
+                        <span>Flex Direction</span>
+                    </a>
+                </li>
+                <li className="navigation__item">
+                    <a href="#" class="navigation__link">
+                        <span>Modern Landing</span>
+                    </a>
+                </li>
+                <li className="navigation__item">
+                    <a href="#" class="navigation__link">
+                        <span>Hover</span>
+                    </a>
+                </li>
             </ul>
             <div className="legal">
                 &copy; 2021 by Jim David
