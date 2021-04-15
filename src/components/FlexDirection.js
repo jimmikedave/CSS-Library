88578170675a285dc98ca57b48f033d1496a4a73
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2021-04-15 13:44:01 -0400

    Update flexbox styling

--- src/components/FlexDirection.js
+++ src/components/FlexDirection.js
@@ -3,11 +3,16 @@ import React from 'react';
 const FlexDirection = () => {
     return (
         <div className="container">
+            <h1 container__heading>Flex Direction</h1>
             <div className="container__content">
-                Flex Direction
+                
+                <div className="flex__container">
+                    <div className="flex__box flex__box--1">One</div>
+                    <div className="flex__box flex__box--2">Two</div>
+                    <div className="flex__box flex__box--3">Three</div>
+                    <div className="flex__box flex__box--4">Four</div>
+                </div>
             </div>
-            {/* Create 4 boxes that will change due to flex direction options */}
-            
         </div>
     )
 }
