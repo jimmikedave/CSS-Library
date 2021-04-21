fea829f71ce212372d097a3d7febca5833434a90
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2021-04-21 10:55:13 -0400

    Add modern landing styling files

--- src/components/ModernLanding.js
+++ src/components/ModernLanding.js
@@ -4,8 +4,16 @@ const ModernLanding = () => {
     //Create the layout for basic modern landing 
     return (
         <div className="container">
+            <h1 container__heading>Modern Landing</h1>
             <div className="container__content">
-                Modern Landing!
+                <div className="modern__container">
+                    <div>Header</div>
+                    <div>Sidebar</div>
+                    <div>Paragraph 1</div>
+                    <div>Paragraph 2</div>
+                    <div>Main Content</div>
+                    <div>Footer</div>
+                </div>
             </div>
         </div>
     )
