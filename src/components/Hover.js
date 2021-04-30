e6c032d6df6556a07bcd87630b60d119f07b4da1
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2021-04-30 11:40:31 -0400

    Update hover component and styling

--- src/components/Hover.js
+++ src/components/Hover.js
@@ -2,8 +2,26 @@ import React from 'react';
 
 const Hover = () => {
     return (
-        <div>
-            Hover!
+        <div className="container">
+            <div className="container__content">
+                <div className="hover__container">
+                    <div className="hover__row">
+                        <div className="hover__box"></div>
+                        <div className="hover__box"></div>
+                        <div className="hover__box"></div>
+                    </div>
+                    <div className="hover__row">
+                        <div className="hover__box"></div>
+                        <div className="hover__box"></div>
+                        <div className="hover__box"></div>
+                    </div>
+                    <div className="hover__row">
+                        <div className="hover__box"></div>
+                        <div className="hover__box"></div>
+                        <div className="hover__box"></div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
