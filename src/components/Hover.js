ee3fbf323bbcd3615125e29f2a686fa2b950cb7b
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2021-05-03 08:07:10 -0400

    Add support classes to hover boxes

--- src/components/Hover.js
+++ src/components/Hover.js
@@ -6,19 +6,19 @@ const Hover = () => {
             <div className="container__content">
                 <div className="hover__container">
                     <div className="hover__row">
-                        <div className="hover__box"></div>
-                        <div className="hover__box"></div>
-                        <div className="hover__box"></div>
+                        <div className="hover__box hover__box--1"></div>
+                        <div className="hover__box hover__box--2"></div>
+                        <div className="hover__box hover__box--3"></div>
                     </div>
                     <div className="hover__row">
-                        <div className="hover__box"></div>
-                        <div className="hover__box"></div>
-                        <div className="hover__box"></div>
+                        <div className="hover__box hover__box--4"></div>
+                        <div className="hover__box hover__box--5"></div>
+                        <div className="hover__box hover__box--6"></div>
                     </div>
                     <div className="hover__row">
-                        <div className="hover__box"></div>
-                        <div className="hover__box"></div>
-                        <div className="hover__box"></div>
+                        <div className="hover__box hover__box--7"></div>
+                        <div className="hover__box hover__box--8"></div>
+                        <div className="hover__box hover__box--9"></div>
                     </div>
                 </div>
             </div>
