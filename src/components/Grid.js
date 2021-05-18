f8404b1fad32ad1fa41fcf4db08b580270d7d21d
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2021-05-18 10:37:49 -0400

    Add grid components and basic styling

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -6,7 +6,15 @@ const Grid = () => {
             <h1 className="container__heading">Modern Landing with Grid</h1>
             <div className="container__content">
                 <div className="grid__container">
-
+                    <div className="grid__header">Header</div>
+                    <div className="grid__sidebar">Sidebar</div>
+                    <div className="grid__main-container">
+                        Main container
+                        <div className="grid__par-one">Paragraph One</div>
+                        <div className="grid__par-two">Paragraph Two</div>
+                        <div className="grid__main-content">Main Content</div>
+                    </div>
+                    <div className="grid__footer">Footer</div>
                 </div>
             </div>
         </div>
